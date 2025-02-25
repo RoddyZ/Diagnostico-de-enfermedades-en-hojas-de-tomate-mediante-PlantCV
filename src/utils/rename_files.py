738c8513bb765b39847c4743608426d094fc3f74
RoddyZ <roddy.zr@hotmail.com>
2025-02-24 21:57:59 -0500

    Reconstruccion con dataNew y mas clases y plantas

--- src/utils/rename_files.py
+++ src/utils/rename_files.py
@@ -1,5 +1,36 @@
 import os
-import shutil
+import sys
+
+def renombrar_carpetas(directorio_principal):
+    """
+    Renombra las carpetas en los directorios 'train' y 'valid' dentro del directorio principal,
+    reemplazando los guiones bajos dobles '___' por uno solo '_', eliminando paréntesis, comas y espacios en blanco.
+
+    Args:
+        directorio_principal (str): Ruta al directorio principal que contiene 'train' y 'valid'.
+    """
+
+    for subdirectorio in ['train', 'valid','test']:
+        ruta_subdirectorio = os.path.join(directorio_principal, subdirectorio)
+
+        if os.path.exists(ruta_subdirectorio):
+            for nombre_carpeta in os.listdir(ruta_subdirectorio):
+                ruta_carpeta_antigua = os.path.join(ruta_subdirectorio, nombre_carpeta)
+
+                if os.path.isdir(ruta_carpeta_antigua):
+                    nombre_carpeta_nuevo = nombre_carpeta.replace(' ', '_')
+                    nombre_carpeta_nuevo = nombre_carpeta_nuevo.replace('___', '_')
+                    nombre_carpeta_nuevo = nombre_carpeta_nuevo.replace('(', '')
+                    nombre_carpeta_nuevo = nombre_carpeta_nuevo.replace(')', '')
+                    nombre_carpeta_nuevo = nombre_carpeta_nuevo.replace(',', '')
+                    ruta_carpeta_nueva = os.path.join(ruta_subdirectorio, nombre_carpeta_nuevo)
+
+                    if nombre_carpeta != nombre_carpeta_nuevo:
+                        os.rename(ruta_carpeta_antigua, ruta_carpeta_nueva)
+                        print(f'Renombrado: {nombre_carpeta} -> {nombre_carpeta_nuevo}')
+        else:
+            print(f'El subdirectorio {subdirectorio} no existe en {directorio_principal}')
+
 
 def rename_images_in_directory(base_directory):
     # Crear las carpetas necesarias para 'train' y 'valid'
